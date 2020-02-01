f8b6f0902a4b456b172c86c386aece30898bb1b8
Edwinvalerio <48640394+Edwinvalerio@users.noreply.github.com>
2020-02-01 02:16:57 -0500

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "199314edwin",
+  password: "123456",
   database: "gofoodapp"
 });
 
